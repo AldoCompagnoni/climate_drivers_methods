0ca73e9db37ee64ccc4e363db0c485de60ea71b7
Compagnoni <ac22qawo@idiv.de>
2023-03-07 17:19:51 +0100

    Simply models, removing all multi-year mods, generalized normal mods, and moving beta mods

--- R/check_bayes.R
+++ R/check_bayes.R
@@ -3,7 +3,7 @@
 # therefore, need be at least work for both airt/precip
 rm(list=ls())
 options(stringsAsFactors=F)
-source("code/format_data.R")
+source("R/format_data.R")
 library(dplyr)
 library(tidyr)
 library(mgcv)
@@ -28,7 +28,8 @@ clim      <- data.table::fread(paste0('data/',clim_var,"_chelsa_prism_hays_2014.
                                stringsAsFactors = F)
 
 # where are the files?
-mod_dir   <- 'E:/work/sApropos/2019.6.11/'
+# mod_dir   <- 'C:/Users/ac22qawo/sapropos_main/out_22.4.2020/'
+mod_dir   <- 'C:/Users/ac22qawo/sapropos_main/out_2021.2/'
 
 # file list
 sum_f_l   <- grep('posterior_',
@@ -42,7 +43,7 @@ path_l    <- paste0(mod_dir, sum_f_l)
 
 # extract species names
 spp       <- gsub("posterior_", "", sum_f_l ) %>%
-                gsub('array_vr-[0-9]{7}-[0-9]{1,2}_', "", . ) %>%
+                gsub('array_vr-[0-9]{7}-[0-9]{1,3}-[0-9]{1,2}_', "", . ) %>%
                 gsub('.csv', "", . ) %>%
                 gsub( paste0('_',resp_l,'_',clim_var),'', .)
  
@@ -102,23 +103,12 @@ bayes_p_val <- function( ii ){
   # transform survival/growth - ONLY if less than 30% data points are 1/0
   if( response == "surv" | response == "grow" | grepl("PreRep", response) | grepl("Rep", response) ){
     
-    raw_x <- mod_data$resp[,response]
-    pc_1  <- sum( raw_x == 1 ) / length(raw_x)
-    pc_0  <- sum( raw_x == 0 ) / length(raw_x)
+    new_y <- mod_data$resp[,response]
+    new_y <- replace( new_y, new_y == 1, 0.99999 )
+    new_y <- replace( new_y, new_y == 0, 0.00001 )
     
-    # for survival
-    if( grepl("surv", response, ignore.case = T) & pc_1 < 0.3 ){
-      n     <- length(raw_x)
-      new_x <- ( raw_x*(n - 1) + 0.5 ) / n
-      mod_data$resp[,response] <- new_x
-    }
-    
-    # for growth
-    if( grepl("grow", response, ignore.case = T) & pc_0 < 0.3 ){
-      n     <- length(raw_x)
-      new_x <- ( raw_x*(n - 1) + 0.5 ) / n
-      mod_data$resp[,response] <- new_x
-    }
+    # replace numbers
+    mod_data$resp[,response] <- new_y
     
   }
   
@@ -155,12 +145,6 @@ bayes_p_val <- function( ii ){
     expp_beta = expp_beta
   )
   
-  # exponential power distribution
-  dexppow <- function(x, mu, sigma, beta) {
-      return((beta / (2 * sigma * gamma (1.0/beta)) ) *
-               exp(-(abs(x - mu)/sigma)^beta));
-  }
-  
   # across models, set up data frames for simulations -----------------------
   
   # models 
@@ -182,6 +166,22 @@ bayes_p_val <- function( ii ){
               y_sd  = post_smpl$y_sd )
   }   
   
+  # select the year's climate
+  sel_clim_yr <- function( mod ){
+    
+    if( grepl('1',mod) ){
+      clim_sub <- as.matrix(mod_data$climate[,c(1:12)]) 
+    }
+    if( grepl('2',mod) ){
+      clim_sub <- as.matrix(mod_data$climate[,c(13:24)])
+    }
+    if( grepl('3',mod) ){
+      clim_sub <- as.matrix(mod_data$climate[,c(25:36)]) 
+    }
+    
+    clim_sub
+    
+  }
   
   # sampled posterior predictive checks across datasets
   sppc <- function(mod, post_l, mod_data){
@@ -204,22 +204,91 @@ bayes_p_val <- function( ii ){
     # function to set up data frames
     if( grepl('yr', mod) ){
         
-      # gaus
-      if(mod=='yr1'){
-        clim_sub <- as.matrix(mod_data$climate[,c(1:12)]) 
-      }
-      if(mod=='yr2'){
-        clim_sub <- as.matrix(mod_data$climate[,c(13:24)])
-      }
-      if(mod=='yr3'){
-        clim_sub <- as.matrix(mod_data$climate[,c(25:36)]) 
-      }
+      # select the climate
+      clim_sub <- sel_clim_yr( mod )
       
       # plotting material & plot data frame
       x_ante <- rowMeans(clim_sub)
       pl_df  <- sppc_df(x_ante, post_l[mod][[1]], mod_data)
       
     }
+    
+    # gaussian model - yearly 
+    if( grepl('gaus[1-3]{1}', mod) ){
+      
+      # select the climate
+      clim_sub <- sel_clim_yr( mod )
+      
+      # mean params
+      post_smpl <- post_l[mod][[1]]
+      
+      # gaus
+      gaus_w_v  <- dnorm(xx, post_smpl$sens_mu, post_smpl$sens_sd )
+      w_v       <- gaus_w_v / sum(gaus_w_v)
+      
+      # plotting material & plot data frame
+      x_ante <- clim_sub %*% w_v
+      pl_df  <- sppc_df(x_ante, post_l[mod][[1]], mod_data)
+      
+    }
+    
+    # SAM yearly
+    if( grepl('simpl[1-3]{1}', mod) ){
+      
+      # select the climate
+      clim_sub <- sel_clim_yr( mod )
+      
+      # let's start from SAD
+      post_smpl <- post_l[mod][[1]]
+      
+      # x antecedent
+      w_v    <- post_smpl[,paste0('theta_',1:12)] %>% as.numeric
+      
+      # plotting material & plot data frame
+      x_ante <- clim_sub %*% w_v
+      pl_df  <- sppc_df(x_ante, post_l[mod][[1]], mod_data)
+      
+    }  
+    
+    # ridge regression
+    if( grepl('ridge[1-3]{1}', mod) ){
+      
+      # select the climate
+      clim_sub <- sel_clim_yr( mod )
+      
+      # let's start from SAD
+      post_smpl <- post_l[mod][[1]]
+      
+      # plotting material & plot data frame
+      x_ante    <- rep(1, nrow(mod_data$climate) )
+      b_mat     <- matrix(select( post_l[mod][[1]], 
+                                  paste0('beta_',1:12) ) %>% unlist,
+                          12, 1)
+      mock_post <- data.frame( alpha = post_l[mod][[1]]$alpha,
+                               beta  = clim_sub %*% b_mat,
+                               y_sd  = post_l[mod][[1]]$y_sd )
+      
+      # put out plot df 
+      pl_df  <- sppc_df(x_ante, mock_post, mod_data)
+      
+    }  
+    
+    # SAM nested
+    if(mod == 'simpl_n'){
+    
+      # let's start from SAD
+      post_smpl <- post_l[mod][[1]]
+      
+      # x antecedent
+      w_v    <- c(as.numeric(post_smpl[,paste0('theta_m_',1:12)]*post_smpl$theta_y_1),
+                  as.numeric(post_smpl[,paste0('theta_m_',1:12)]*post_smpl$theta_y_2),
+                  as.numeric(post_smpl[,paste0('theta_m_',1:12)]*post_smpl$theta_y_3) )
+      
+      # plotting material & plot data frame
+      x_ante <- as.matrix(mod_data$climate) %*% w_v
+      pl_df  <- sppc_df(x_ante, post_l[mod][[1]], mod_data)
+          
+    }  
     
     # gaussian model
     if(mod == 'gaus'){
@@ -237,81 +306,8 @@ bayes_p_val <- function( ii ){
       
     }
     
-    # Generalized Normal model
-    if(mod == 'expp'){
-      
-      # mean params
-      post_smpl <- post_l[mod][[1]]
-      
-      # expp
-      expp_w_v  <- dexppow(xx_a, post_smpl$sens_mu, 
-                                 post_smpl$sens_sd,
-                                 expp_beta)
-      w_v       <- expp_w_v / sum(expp_w_v)
-      
-      # plotting material & plot data frame
-      x_ante <- as.matrix(mod_data$climate) %*% w_v
-      pl_df  <- sppc_df(x_ante,post_l[mod][[1]],mod_data)
-    
-    }
-    
-    # Nested Gaussian
-    if(mod == 'gaus_n'){
-    
-      # let's start from SAD
-      post_smpl <- post_l[mod][[1]]
-      
-      # gaus
-      gaus_w_v <- dnorm(xx, post_smpl$sens_mu, post_smpl$sens_sd)
-      w_v      <- c( ((gaus_w_v / sum(gaus_w_v))*post_smpl$theta_y_1),
-                     ((gaus_w_v / sum(gaus_w_v))*post_smpl$theta_y_2),
-                     ((gaus_w_v / sum(gaus_w_v))*post_smpl$theta_y_3) )
-     
-      # plotting material & plot data frame
-      x_ante <- as.matrix(mod_data$climate) %*% w_v
-      pl_df  <- sppc_df(x_ante, post_l[mod][[1]], mod_data)
-        
-    }  
-    
-    # Nested Generalized Normal
-    if(mod == 'expp_n'){
-    
-      # let's start from SAD
-      post_smpl <- post_l[mod][[1]]
-      
-      # expp
-      expp_w_v <- dexppow(xx, post_smpl$sens_mu, 
-                              post_smpl$sens_sd, 
-                              expp_beta)
-      w_v <- c( ((expp_w_v / sum(expp_w_v))*post_smpl$theta_y_1),
-                ((expp_w_v / sum(expp_w_v))*post_smpl$theta_y_2),
-                ((expp_w_v / sum(expp_w_v))*post_smpl$theta_y_3) )
-     
-      # plotting material & plot data frame
-      x_ante <- as.matrix(mod_data$climate) %*% w_v
-      pl_df  <- sppc_df(x_ante, post_smpl, mod_data)
-        
-    }  
-    
-    # SAM
-    if(mod == 'simpl_n'){
-    
-      # let's start from SAD
-      post_smpl <- post_l[mod][[1]]
-      
-      # x antecedent
-      w_v    <- c(as.numeric(post_smpl[,paste0('theta_m_',1:12)]*post_smpl$theta_y_1),
-                  as.numeric(post_smpl[,paste0('theta_m_',1:12)]*post_smpl$theta_y_2),
-                  as.numeric(post_smpl[,paste0('theta_m_',1:12)]*post_smpl$theta_y_3) )
-      
-      # plotting material & plot data frame
-      x_ante <- as.matrix(mod_data$climate) %*% w_v
-      pl_df  <- sppc_df(x_ante, post_l[mod][[1]], mod_data)
-          
-    }  
-    
-    # moving beta hierarchical
-    if( mod == 'mb_h' | mod == 'mb' ){
+    # ridge regression
+    if( mod == 'ridge' ){
     
       # let's start from SAD
       post_smpl <- post_l[mod][[1]]
@@ -335,37 +331,7 @@ bayes_p_val <- function( ii ){
           
     }  
     
-    # nested moving beta hierarchical
-    if( mod == 'mb_h_n' | mod == 'mb_n' ){
-    
-      # let's start from SAD
-      post_smpl <- post_l[mod][[1]]
-      
-      # plotting material & plot data frame
-      x_ante    <- rep(1, nrow(mod_data$climate) )
-      b_m_mat   <- matrix(select( post_l[mod][[1]], 
-                                  paste0('beta_',1:12) ) %>% unlist,
-                          12, 1)
-      
-      b_mat     <- do.call( rbind, 
-                            list( b_m_mat * post_l[mod][[1]]$theta_y_1,
-                                  b_m_mat * post_l[mod][[1]]$theta_y_2,
-                                  b_m_mat * post_l[mod][[1]]$theta_y_3 ) )
-      
-      c_mat     <- as.matrix(mod_data$climate)
-      mock_post <- data.frame( alpha = post_l[mod][[1]]$alpha,
-                               beta  = c_mat %*% b_mat,
-                               y_sd  = post_l[mod][[1]]$y_sd ) #%>% 
-                      # test computations are correct
-                      # mutate( yhat   = select(post_l[mod][[1]], 
-                      #                         paste0('yhat_',1:31) ) %>% unlist,
-                      #         pred   = alpha + beta )
-                    
-      # put out plot df 
-      pl_df  <- sppc_df(x_ante, mock_post, mod_data)
-          
-    }
-    
+  
     # Bayesian p-value calculation: sample posterior
     sim_post <- function(ss){
       dis_df <- pl_df %>% 
@@ -400,10 +366,10 @@ bayes_p_val <- function( ii ){
   # calculate Bayesian p-value
   lapply(c('ctrl1', 
            'yr1','yr2','yr3',
-           'gaus',     'expp',
-           'gaus_n',   'expp_n','simpl_n',
-           'mb_h',   'mb',
-           'mb_h_n', 'mb_n'), 
+           'gaus1','gaus2','gaus3',
+           'simpl1','simpl2','simpl3',
+           'ridge1','ridge2','ridge3',
+           'gaus',  'simpl_n','ridge'), 
           sppc, post_l, mod_data) %>% 
     bind_rows %>% 
     mutate( response = cases_df$resp[ii],
@@ -460,18 +426,26 @@ bayes_p %>%
 # make one big graph! -----------------------------------------
 
 # order of models
-mod_ordr <- c('ctrl1',  'yr1', 'yr2', 'yr3',
-              'gaus',   'expp',
-              'gaus_n', 'expp_n', 'simpl_n',
-              'mb_h',    'mb',
-              'mb_h_n',  'mb_n')
+mod_ordr <- c('ctrl1', 
+              'yr1','yr2','yr3',
+              'gaus1','gaus2','gaus3',
+              'simpl1','simpl2','simpl3',
+              'ridge1','ridge2','ridge3',
+              'gaus',  'simpl_n','ridge')
+
+update  <- data.frame( mod   = mod_ordr[1:13],
+                       model = c('NM',
+                                 'CSM 1','CSM 2','CSM 3',
+                                 'WMM 1','WMM 2','WMM 3',
+                                 'SAM 1','SAM 2','SAM 3',
+                                 'FHM 1','FHM 2','FHM 3') )
 
 
 bayes_at  <- readRDS( paste0('results/checks/airt_p_val.rds') ) %>%
-                mutate( mod = as.character(mod) ) %>% 
+                mutate( mod = as.character(mod) ) %>%
                 mutate( mod = factor(mod, levels = mod_ordr) )
 bayes_pr  <- readRDS( paste0('results/checks/precip_p_val.rds') ) %>%
-                mutate( mod = as.character(mod) ) %>%  
+                mutate( mod = as.character(mod) ) %>%
                 mutate( mod = factor(mod, levels = mod_ordr) )
 bayes_all <- bind_rows( bayes_at, bayes_pr ) %>% 
                 mutate( clim_var = replace(clim_var, 
@@ -479,23 +453,28 @@ bayes_all <- bind_rows( bayes_at, bayes_pr ) %>%
                                            'Air temperature') ) %>% 
                 mutate( clim_var = replace(clim_var, 
                                            clim_var == 'precip',
-                                           'Precipitation') ) 
+                                           'Precipitation') ) %>% 
+                subset( !(mod %in% c('gaus','simpl_n','ridge')) ) %>% 
+                left_join( update ) %>% 
+                mutate( model = factor(model, levels = update$model) )
 
 
 # p_value by model 
-ggplot(bayes_all, 
-       aes( x = mod,
-            y = p_val,
-            color = clim_var) ) +
+ggplot( bayes_all, 
+        aes(  x = model,
+              y = p_val,
+              color = clim_var) ) +
   geom_point( position = position_jitter(w=0.06,h=0),
               alpha = 0.5) +
   geom_hline( yintercept = 0.025, lty=2  ) +
   geom_hline( yintercept = 0.975, lty=2  ) +
   scale_color_viridis_d() +
+  theme_bw() +
   theme( axis.text.x = element_text( angle=90,
                                      hjust=1,
                                      vjust=0.5),
          legend.title = element_text("Clim. var.") ) +
+  
   ylab( 'Bayesian P-value' ) +
   xlab( 'Model' ) +
   labs( color = 'Predictor') +
